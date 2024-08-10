6ea2794c83d48bb8e7a3370f60e0096e58553e31
Oleksandr Pavlyk <oleksandr.pavlyk03@gmail.com>
2024-08-10 17:42:00 +0300

    mmore

---
+++ OppositeNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OppositeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new Program();
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(num.Opposite(5));
+        }
+
+        public int Opposite(int number)
+        {
+            return number * -1;
+        }
+    }
+}
---
+++ Return the day/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Return_the_day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static string ReturnDay(int day)
+        {
+            string today = String.Empty;
+            switch (day)
+            {
+                case 1:
+                    today = "Monday";
+                    break;
+                case 2:
+                    today = "Tuesday";
+                    break;
+                case 3:
+                    today = "Wednesday";
+                    break;
+                case 4:
+                    today = "Thursday";
+                    break;
+                case 5:
+                    today = "Friday";
+                    break;
+                case 6:
+                    today = "Saturday";
+                    break;
+                case 7:
+                    today = "Sunday";
+                    break;
+            }
+            return today;
+        }
+    }
+}
